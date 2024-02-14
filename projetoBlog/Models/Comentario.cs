fc4fc9c6fbac7caca3103c66870ed6e67b6010bc
Rafael Menezes <rafa.rcm@hotmail.com>
2024-02-14 09:48:16 -0300

    Criando as classes

--- projetoBlog/Models/Comentario.cs
+++ projetoBlog/Models/Comentario.cs
@@ -1,13 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace projetoBlog.Models
 {
     public class Comentario
     {
-        // XXX TRABALHE AQUI
-        // Crie aqui a clase de comentários
+        public string Autor { get; set; }
+        public string Conteudo { get; set; }
+        public DateTime DataCriacao { get; set; }
     }
 }
